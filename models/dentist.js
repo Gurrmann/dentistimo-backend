6582f8670784f7f139d7c1bc7ba2ba5a97bc817b
Emil Gustafsson <emgusta@student.chalmers.se>
2020-11-17 18:34:00 +0100

    Add appointment model

--- models/dentist.js
+++ models/dentist.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var dentistSchema = new Schema ({
-    dentist_name : {type: String, required: true, unique: true}
+    dentist_name : {type: String, required: true}
 });
 
 module.exports = mongoose.model('dentists', dentistSchema);
