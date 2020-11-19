2343a0213e05bd0c2aa47830cbe46ecdec38ea0b
matekd <matekd@student.chalmers.se>
2020-11-19 17:07:46 +0100

    Make attribute reference correct model

--- models/appointment.js
+++ models/appointment.js
@@ -5,7 +5,7 @@ var appointmentSchema = new Schema ({
     type : {type: String, required: true},
     id : {type: Number, required: true, unique: true},
     time_slot : {type: Schema.Types.ObjectId, ref: 'Time_slot', required: true},
-    dentistry : {type: Schema.Types.ObjectId, ref: 'Time_slot', required: true}
+    dentistry : {type: Schema.Types.ObjectId, ref: 'Dentistry', required: true}
 });
 
 module.exports = mongoose.model('appointments', appointmentSchema);
