d1349d62f8d0967be273c0bb07d702b4bbd8bc0b
Mattias Ekdahl <matekd@student.chalmers.se>
2020-11-20 21:22:19 +0100

    Moved subscriptions and publish to backend

--- backend.js
+++ backend.js
@@ -1,15 +1,14 @@
 //Load HTTP module
-const http = require("http");
+/*const http = require("http");
 const hostname = '127.0.0.1';
-const port = 3000;
-
+const port = 3000;*/
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/dentistimoDB')
 var db = mongoose.connection;
 
 //Create HTTP server and listen on port 3000 for requests
-const server = http.createServer((req, res) => {
+/*const server = http.createServer((req, res) => {
 
   //Set the response HTTP header with HTTP status and Content type
   res.statusCode = 200;
@@ -20,6 +19,33 @@ const server = http.createServer((req, res) => {
 //listen for request on port 3000, and as a callback function have the port listened on logged
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-});
+});*/
 
+var mqtt = require('mqtt')
+var client  = mqtt.connect('mqtt://test.mosquitto.org')
+var dentistry = require('./models/dentistry')
 
+/* dentistry.find({}, function (err, result) {
+  if (err) return handleError(err);
+  if (result) {
+      message = result[0].name
+  }
+}) */
+
+client.on('connect', function () {
+  client.subscribe('dentistry')
+  publish('dentistry', '{"msg": "Hello internet"}')
+})
+
+client.on('message', function (topic, message) {
+  // message is Buffer
+  message = JSON.parse(message)
+  console.log(message.msg)
+  //client.end()
+})
+
+function publish(topic, message) {
+  client.publish(topic, message)
+}
+
+//client.end()
