e6a8030b5c0e85ab8e902cb8546fc8a209f2eb80
simengs <simengs@student.chalmers.se>
2020-11-24 17:21:29 +0100

    renamed backend to databaseHandler, and renamed loadDentistries to loadData

---
+++ databaseHandler.js
@@ -0,0 +1,47 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/dentistimoDB')
+var db = mongoose.connection;
+
+var mqtt = require('mqtt')
+//var client  = mqtt.connect('mqtt://test.mosquitto.org')
+var client  = mqtt.connect('mqtt://localhost:1883')
+var Dentistry = require('./models/dentistry')
+
+client.on('connect', function () {
+  client.subscribe('dentistry')
+  publish('dentistry', '{"msg": "Hello internet"}')
+})
+
+client.on('message', function (topic, message) {
+  // message is Buffer
+  message = JSON.parse(message)
+  console.log(message.msg)
+  //client.end()
+})
+
+function publish(topic, message) {
+  client.publish(topic, message)
+}
+
+//client.end()
+
+readDentistries()
+
+function createDentistry(data){
+  var dentistry = new Dentistry(data)
+  dentistry.save(function(err, result){
+    if(err){
+      console.log(err)
+    }
+  })
+}
+
+function readDentistries(){
+  Dentistry.find(function(err, result){
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(result)
+    }
+  })
+}
---
+++ loadData.js
@@ -0,0 +1,85 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/dentistimoDB')
+var Dentistry = require("./models/dentistry")
+
+createDentistry({
+  name   : "Your Dentist",
+  adress : "Spannmålsgatan 20",
+  owner  : "Dan Tist",
+  city   : "Gothenburg",
+  dentists : 1,
+  coordinates : ({
+      longitude : 11.969388,
+      latitude  : 57.707619
+  }),
+  opening_hours : ({
+    monday : "9:00 - 17:00",
+    tuesday : "8:00 - 17:00",
+    wednesday : "7:00 - 16:00",
+    thursday : "9:00 - 17:00",
+    friday:  "9:00 - 15:00"
+  })
+})
+
+createDentistry({
+    name   : "Tooth Fairy Dentist",
+    adress : "Slottsskogen",
+    owner  : "Tooth Fairy",
+    city   : "Gothenburg",
+    dentists : 1,
+    coordinates : ({
+        longitude : 11.942625,
+        latitude  : 57.685255
+    }),
+    opening_hours : ({
+      monday : "7:00 - 19:00",
+      tuesday : "7:00 - 19:00",
+      wednesday : "7:00 - 19:00",
+      thursday : "7:00 - 19:00",
+      friday:  "7:00 - 19:00"
+    })
+  })
+
+  createDentistry({
+    name   : "The Crown",
+    adress : "Lindholmsallén 19",
+    owner  : "Carmen Corona",
+    city   : "Gothenburg",
+    dentists : 1,
+    coordinates : ({
+        longitude : 11.940386,
+        latitude  : 57.709872
+    }),
+    opening_hours : ({
+      monday : "6:00 - 15:00",
+      tuesday : "8:00 - 17:00",
+      wednesday : "7:00 - 12:00",
+      thursday : "7:00 - 17:00",
+      friday:  "8:00 - 16:00"
+    })
+  })
+
+setTimeout(readDentistries, 1000)
+
+//Dentistry.deleteMany({}, function(err, result){ console.log(result) })
+
+function createDentistry(data){
+  var dentistry = new Dentistry(data)
+  dentistry.save(function(err, result){
+    if(err){
+      console.log(err)
+    }
+  })
+}
+
+function readDentistries(){
+  Dentistry.find(function(err, result){
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(result)
+    }
+  })
+}
+
+//function createDentistry(data) { console.log(1) }
