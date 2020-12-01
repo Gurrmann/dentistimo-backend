472bde7595ff5f57d2c54455693ea2ce9b182ad7
Gustav Skallberg <gustav.skallberg@gmail.com>
2020-12-01 22:06:28 +0100

    Change in model to correctly represent the data

--- models/appointment.js
+++ models/appointment.js
@@ -2,9 +2,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var appointmentSchema = new Schema ({
-    patient_name : {type: String, required: true},
-    time : {type: String, required: true},
-    dentistry : {type: Schema.Types.ObjectId, ref: 'Dentistry', required: true}
+
+    userId : {type: Number, required: true},
+    time_slot : {type: Number, required: true},
+    dentistry : {type: String, required: true}
 });
 
 module.exports = mongoose.model('appointments', appointmentSchema);
